7fcdaedbf6ae6f04f6f6042ced2d2b93f9604c04
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2022-01-18 22:20:24 -0600

    Header added, first two input added
    Need to find variant descriptions

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,23 +1,80 @@
 import React from 'react';
 
-//Mantine hooks
-import { Card, Grid, Text } from '@mantine/core';
+//Stylesheet
+import './MainPage.css';
+
+//MathJax
+import { MathJaxContext, MathJax } from 'better-react-mathjax';
+
+//Mantine
+import { Button, Card, Grid, NumberInput, Text } from '@mantine/core';
+
+const config = {
+   loader: { load: ["[tex]/html"] },
+   tex: {
+     packages: { "[+]": ["html"] },
+     inlineMath: [
+       ["$", "$"],
+       ["\\(", "\\)"]
+     ],
+     displayMath: [
+       ["$$", "$$"],
+       ["\\[", "\\]"]
+     ]
+   }
+ };
 
 //Component content
 const MainPage = () => {
-   
+
    return (
       <>
          <header>
             <Card>
                <Grid>
                   <Grid.Col>
-                     <Text align="center" size="xl" weight={700} >Difracción</Text>
-                     <Text align="center" size="md" >d·sen&theta;=m·&lambda;</Text>
+                     <Text align="center" size="xl" weight={700} >Red de difracción</Text>
+                     <MathJaxContext version={3} config={config} >
+                        <div>
+                           <MathJax hideUntilTypeset={"first"} >
+                              {`\\[a·sen(θ)=\\pm m·λ\\]`}
+                           </MathJax>
+                        </div>
+                     </MathJaxContext>
                   </Grid.Col>
                </Grid>
             </Card>
          </header>
+         <br />
+         <main>
+            <Grid justify="center" >
+               <Grid.Col span={10} >
+                  <Card>
+                     <Text align="center" size="lg" weight={700} >Ángulo de difracción</Text>
+                     <MathJaxContext version={3} config={config} >
+                        <div>
+                           <MathJax hideUntilTypeset={"first"} >
+                              {`\\[θ=tan^{-1}\\left(\\frac{x}{L}\\right)\\]`}
+                           </MathJax>
+                        </div>
+                     </MathJaxContext>
+                     <Grid>
+                        <Grid.Col span={6} >
+                           <NumberInput label="Longitud de onda" variant="filled" icon={"x"} min={0} rightSection="cm." />
+                        </Grid.Col>
+                        <Grid.Col span={6} >
+                           <NumberInput label="Longitud de onda" variant="filled" icon={"L"} min={0} rightSection="m." />
+                        </Grid.Col>
+                     </Grid>
+                     <Grid >
+                        <Grid.Col className='button-div' span={12} >
+                           <Button variant='default' color="gray" size="md" >Calcular</Button>
+                        </Grid.Col>
+                     </Grid>
+                  </Card>
+               </Grid.Col>
+            </Grid>
+         </main>
       </>
    );
 };
